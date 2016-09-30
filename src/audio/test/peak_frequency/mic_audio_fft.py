a467c14702e973bb83c42f77861bf6b38d44bf4b
Ryan Coppa <ryan.coppa@gmail.com>
2016-09-30 10:12:48 -0500

    Support for finding multiple frequencies

--- src/audio/test/peak_frequency/mic_audio_fft.py
+++ src/audio/test/peak_frequency/mic_audio_fft.py
@@ -9,6 +9,10 @@ RATE = 44100
 CHUNK = 1024
 RUN = True
 
+# Make these values configurable!
+LOW_BIN_CUTOFF=1000
+HIGH_BIN_CUTOFF=3000
+
 # Setup a signal handler to catch Ctrl+C to exit the program
 def signal_handler(signal, frame):
     RUN = False
@@ -28,17 +32,28 @@ while RUN is True:
 
     # Take the FFT of the data
     fft = numpy.fft.fft(data)
+    fftBins = len(fft)
 
     # Obtain the sample frequencies that correspond to the FFT samples
-    freqs = numpy.fft.fftfreq(len(fft))
+    freqs = numpy.fft.fftfreq(fftBins)
 
-    # Find the peak frequency
-    peakIndex = numpy.argmax(numpy.abs(fft))
+    # Sort the data based on provided cutoff values
+    binResolution = (RATE / 2) / fftBins
 
-    # Convert the sample frequency into the actual frequency
-    peakFreq = abs(freqs[peakIndex] * RATE)
+    lowBinIndex = int(LOW_BIN_CUTOFF / binResolution)
+    highBinIndex = int(HIGH_BIN_CUTOFF / binResolution)
 
-    print(peakFreq)
+    # Find the peak frequencies (Sample the same number of frequencies per bin)
+    lowPeakIndex = numpy.argmax(numpy.abs(fft[:lowBinIndex - 1]))
+    midPeakIndex = numpy.argmax(numpy.abs(fft[lowBinIndex:highBinIndex - 1]))
+    highPeakIndex = numpy.argmax(numpy.abs(fft[highBinIndex:highBinIndex + (highBinIndex - lowBinIndex)]))
+
+    # Convert the sample frequencies into the actual frequencies
+    lowPeakFreq = abs(freqs[lowPeakIndex] * RATE)
+    midPeakFreq = abs(freqs[lowBinIndex + midPeakIndex] * RATE)
+    highPeakFreq = abs(freqs[highBinIndex + highPeakIndex] * RATE)
+
+    print("low = %d mid = %d high = %d" % (lowPeakFreq, midPeakFreq, highPeakFreq))
 
 # Cleanup
 stream.stop_stream()
