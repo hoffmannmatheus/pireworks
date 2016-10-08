f2cdedea877799c4ea4317901ceb84a434720650
Ezhilarasi Swaminathan <ezhilarasi.swaminathan@gmail.com>
2016-10-08 15:02:22 -0400

    Implement Bluetooth communication in App

--- src/app/pireworks/app/src/androidTest/java/com/pireworks/app/pireworks/ExampleInstrumentedTest.java
+++ src/app/pireworks/app/src/androidTest/java/com/pireworks/app/pireworks/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.pireworks.app.pireworks", appContext.getPackageName());
+        assertEquals("com.Pireworks.app.Pireworks", appContext.getPackageName());
     }
 }
