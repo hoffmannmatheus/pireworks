4c0522af6cf3630139e15c6e4e74d702bcf4145a
Ryan Coppa <ryan.coppa@gmail.com>
2016-10-26 21:22:34 -0500

    Exception improvement from Fei

--- src/audio/src/core_audio.py
+++ src/audio/src/core_audio.py
@@ -66,8 +66,12 @@ class AudioInput(Thread):
         """The thread function"""
         while self.running is True:
             if self.wav is None:
-                # Read audio samples from the audio stream
-                data = numpy.fromstring(self.stream.read(self.chunk_size), dtype=numpy.int16)
+                try:
+                    # Read audio samples from the audio stream
+                    data = numpy.fromstring(self.stream.read(self.chunk_size), dtype=numpy.int16)
+                except BufferError:
+                    # Buffer errors are not fatal, continue
+                    continue
             else:
                 # Read audio samples from input file
                 if self.wav.tell() >= self.wav.getnframes():
