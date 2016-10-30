3ed8dd7921c4ee4e84576e940943d86774893753
Matt Silva <matt.silva@kiswe.com>
2016-10-30 19:44:04 -0400

    [backend] Added database schema.

---
+++ src/backend/src/data/schema.sql
@@ -0,0 +1,47 @@
+
+--
+-- Create database table, if needed.
+--
+CREATE TABLE IF NOT EXISTS configuration ( 
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    is_default INTEGER DEFAULT 0 NOT NULL,
+    name VARCHAR(80) UNIQUE NOT NULL,
+	cutoff_freqs TEXT NOT NULL,
+	trigger_threshold INTEGER NOT NULL,
+	trigger_offset INTEGER NOT NULL,
+	scaled_max_value INTEGER NOT NULL,
+	output_binary INTEGER NOT NULL,
+	chunk INTEGER NOT NULL,
+	rate INTEGER NOT NULL
+); 
+
+--
+-- (Re)Insert default configuration.
+--
+REPLACE INTO configuration (
+	id,
+	is_default,
+	name,
+	cutoff_freqs,
+	trigger_threshold,
+	trigger_offset,
+	scaled_max_value,
+	output_binary,
+	chunk,
+	rate
+) VALUES (
+	0,  			  	-- id
+	1,					-- is_default
+	"default",			-- name
+	"800,1500,3000",	-- cutoff_freqs, should be comma separated
+	150000,				-- trigger_threshold
+	150000,				-- trigger_offset
+	255,				-- scaled_max_value
+	1,					-- output_binary
+	512,				-- chunk
+	44100				-- rate
+);
+
+--
+-- TODO: Insert more preset configurations, like 'noisy room' or 'quiet room'
+--
