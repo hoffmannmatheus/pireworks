35d5e5fa5f3a1e8183fbd20f7d1fb8dcbf7129c6
Ezhilarasi Swaminathan <ezhilarasi.swaminathan@gmail.com>
2016-11-08 09:22:55 -0500

    Update DeviceListActivity.java

--- src/app/pireworks/app/src/main/java/com/pireworks/app/pireworks/DeviceListActivity.java
+++ src/app/pireworks/app/src/main/java/com/pireworks/app/pireworks/DeviceListActivity.java
@@ -20,27 +20,42 @@ public class DeviceListActivity extends Activity implements DeviceListAdapter.On
     public static final String EXTRA_DEVICE_LIST = "bluetooth_device_list";
 
     private ListView mListView;
-    private DeviceListAdapter mAdapter;
+    private DeviceListAdapter mAdapter; 
     private ArrayList<BluetoothDevice> mDeviceList;
 
+    /**  Perform Initial Application Activities -
+         1. Get UI elements from the xml and set the content for the activity
+         2. Get default Bluetooth adapter 
+         3. Register the broadcast receiver.
+    **/
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        
         setContentView(R.layout.activity_paired_devices);
 
+        //populate bluetooth device list
         mDeviceList	= getIntent().getExtras().getParcelableArrayList(EXTRA_DEVICE_LIST);
+        
+        //create list view for paired devices
         mListView = (ListView) findViewById(R.id.lv_paired);
+        
+        //get default blueetooth adapter and add devices to the list at runtime
         mAdapter = new DeviceListAdapter(this);
-
         mAdapter.setData(mDeviceList);
         mAdapter.setListener(this);
 
         mListView.setAdapter(mAdapter);
-
+        
+        //register the broadcast reciever so that it can be invoked when the bluetooth state is changed
         registerReceiver(mPairReceiver, new IntentFilter(BluetoothDevice.ACTION_BOND_STATE_CHANGED));
     }
 
+    
+    /** Unregister the broadcast receiver to avoid memmory leaks 
+        and then
+        destroy the activity
+    **/
     @Override
     public void onDestroy() {
         unregisterReceiver(mPairReceiver);
@@ -48,11 +63,15 @@ public class DeviceListActivity extends Activity implements DeviceListAdapter.On
         super.onDestroy();
     }
 
-
+    /** Display text like scanning/discovering devices
+        w.r.t the Bluetooth Application context
+    **/
     private void showToast(String message) {
         Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
-    }
-
+    
+    /** Use BluetoothDevice's createBond() 
+        to pair the selected device 
+    **/
     private void pairDevice(BluetoothDevice device) {
         try {
             Method method = device.getClass().getMethod("createBond", (Class[]) null);
@@ -61,7 +80,10 @@ public class DeviceListActivity extends Activity implements DeviceListAdapter.On
             e.printStackTrace();
         }
     }
-
+   
+    /** Use BluetoothDevice's removeBond() 
+        to unpair the selected device
+    **/
     private void unpairDevice(BluetoothDevice device) {
         try {
             Method method = device.getClass().getMethod("removeBond", (Class[]) null);
@@ -72,6 +94,9 @@ public class DeviceListActivity extends Activity implements DeviceListAdapter.On
         }
     }
 
+    /** Create the BroadcastReceiver object to recieve BroadCastIntent from Bluetooth devices
+        and show toast when the state of the bluetooth devices change
+    **/
     private final BroadcastReceiver mPairReceiver = new BroadcastReceiver() {
         public void onReceive(Context context, Intent intent) {
             String action = intent.getAction();
@@ -91,6 +116,9 @@ public class DeviceListActivity extends Activity implements DeviceListAdapter.On
         }
     };
 
+    /** Add the selected bluetooth device to the pireworks_device_list
+        and launch Pirewroks Activity
+    **/
     @Override
     public void onDeviceClick(BluetoothDevice device) {
         if (device == null) {
@@ -101,6 +129,9 @@ public class DeviceListActivity extends Activity implements DeviceListAdapter.On
         startActivity(newIntent);
     }
 
+    /** Pair the selected bluetooth device if it is not already paired
+        else unpair device
+    **/
     @Override
     public void onPairButtonClick(BluetoothDevice device) {
 
