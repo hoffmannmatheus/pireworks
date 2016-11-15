7fc84917fa1c22576171516e8bf2ebcba1ea4feb
Ryan Coppa <ryan.coppa@gmail.com>
2016-11-14 21:48:34 -0600

    Increase fidelity of audio processing

--- src/audio/test/core_audio_test.py
+++ src/audio/test/core_audio_test.py
@@ -9,8 +9,8 @@ def callback_function(values):
     print(values)
 
 # Create the audio class
-#audio = CoreAudio()
-audio = CoreAudio("audio_sweep.wav")
+audio = CoreAudio()
+#audio = CoreAudio("audio_sweep.wav")
 
 # Register your callback
 audio.register(callback_function)
@@ -20,7 +20,7 @@ audio.start()
 
 # Test reconfigure
 audio.stop()
-audio.configure(output_binary=False)
+audio.configure(cutoff_freqs=[65, 73, 82, 87, 98, 110, 123, 131, 147, 165, 175, 196, 220, 247, 262, 294, 330, 349, 392, 440, 494, 523, 587, 659, 698, 784, 880, 988, 1046, 1175, 1319, 1397, 1568, 1760, 1975, 2093, 2349, 2637, 2794, 3136, 3520, 3951, 4186, 4699, 5274, 5588, 6272, 7040, 7902])
 audio.start()
 
 # Reset configuration
