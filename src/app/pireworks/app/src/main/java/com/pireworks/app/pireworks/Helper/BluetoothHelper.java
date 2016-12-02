98c311b5349b6b1b7c22c155e8be7f8c2376f620
Ezhilarasi Swaminathan <ezhilarasi.swaminathan@gmail.com>
2016-12-01 21:04:43 -0500

    Android Config Changes

--- src/app/pireworks/app/src/main/java/com/pireworks/app/pireworks/Helper/BluetoothHelper.java
+++ src/app/pireworks/app/src/main/java/com/pireworks/app/pireworks/Helper/BluetoothHelper.java
@@ -25,9 +25,25 @@ public class BluetoothHelper {
              * actual string, so it can appear in the text field.
              */
 
+
+
+            JsonObject jsonColors=new JsonObject();
+            jsonColors.addProperty("C",  config.getColHashMap().get("C"));
+            jsonColors.addProperty("D",  config.getColHashMap().get("D"));
+            jsonColors.addProperty("E",  config.getColHashMap().get("E"));
+            jsonColors.addProperty("F",  config.getColHashMap().get("F"));
+            jsonColors.addProperty("G",  config.getColHashMap().get("G"));
+            jsonColors.addProperty("A",  config.getColHashMap().get("A"));
+            jsonColors.addProperty("B",  config.getColHashMap().get("B"));
+
+            JsonObject jsonConfig=new JsonObject();
+            jsonConfig.addProperty("trigger_threshold", config.getTriggerThreshold());
+            jsonConfig.addProperty("trigger_offset", config.getTriggerOffset());
+            jsonConfig.add("colors", jsonColors );
+
             JsonObject builder=new JsonObject();
             builder.addProperty("action","set");
-            builder.addProperty("config",config.toString());
+            builder.add("config", jsonConfig);
             String jsonMessage=(new Gson()).toJson(builder);
 
             try{
